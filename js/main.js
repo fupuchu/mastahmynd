7400578db55848d44588fd52de4fca1b7a2008a6
Leon Ho <leonhwh@icloud.com>
2018-06-12 00:41:22 +0800

    Two Functions Added attemptCheck(); compareArray(); - TBD add .data to put in datatypes last test: attempCheck() working

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 var ansObj = {
+  num0 : Math.floor(Math.random() * 10).toString(),
   num1 : Math.floor(Math.random() * 10).toString(),
   num2 : Math.floor(Math.random() * 10).toString(),
-  num3 : Math.floor(Math.random() * 10).toString(),
-  num4 : Math.floor(Math.random() * 10).toString()
+  num3 : Math.floor(Math.random() * 10).toString()
 }
 
 console.log(ansObj);
 
 var userGuess = {
+  num0 : null,
   num1 : null,
   num2 : null,
-  num3 : null,
-  num4 : null
+  num3 : null
 }
 
 var attempts = 0;
@@ -19,21 +19,37 @@ var attempts = 0;
 function checkNum(num){
   var x = num.id;
   var ans = ansObj[x]
+  console.log(num);
   if (num.value === ans) {
     console.log("Match");
     changeAttr(x);
     $('#' + x).addClass('correct')
+    $('#' + x).data('state', 'correct')
     userGuess[x] = num.value;
+    var currState = $('#' + x).data("state")
+    console.log(currState);
+    attemptCheck(currState);
   }else {
     console.log("No Match");
     changeAttr(x);
     $('#' + x).addClass('wrong')
+    $('#' + x).data('state', "wrong")
     userGuess[x] = num.value;
+    var currState = $('#' + x).data("state")
+    console.log(currState);
+    attemptCheck(currState);
   }
 }
 
-// JSON.stringify
-
+function attemptCheck(x){
+  if ( x === 'correct') {
+    console.log("Don't Change it");
+  }else {
+    console.log("Change this");
+  }
+}
+//document.querySelector('#gameBoard').getAttribute('data-info')
+// if class is == correct check for win classes and if classes = 4 , you win! else allow input to be edited again
 function changeAttr(y){
   $('#' + y).attr("readonly", true);
 }
--- js/win.js
+++ js/win.js
@@ -1,3 +1,18 @@
-function win(){
+var arr1 = ["1","2","3","4"]
+var arr2 = ["1","2","3","5"]
 
+
+
+function compareArray() {
+  if (arr1.length == arr2.length) {
+    for (var i = 0; i < arr2.length; i++) {
+      if (arr1[i] == arr2[i]) {
+        console.log(arr1[i] + " is equals to " + arr2[i]);
+      }else{
+        console.log(arr1[i] + " is not equals to " + arr2[i]);
+      }
+    }
+  }else{
+    console.log("Array not filled");
+  }
 }
