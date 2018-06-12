150a461b59fa29d492b26f3eba972a3bb3a0c2a4
Leon Ho <leonhwh@icloud.com>
2018-06-12 17:08:36 +0800

    Finish checkWin() Working on resetGame()

--- js/win.js
+++ js/win.js
@@ -8,23 +8,35 @@ function compareArray() {
   var x = arr2.join("")
   $("#history").append("<span>" + x + "</span>")
   $("#attemptBox").text(attempts);
-  if(attempts === 10){
+  if (attempts === 10) {
     console.log("You lose...");
     $('.btn').remove();
-  }else{
+  } else {
     if (arr1.length == arr2.length) {
       for (var i = 0; i < arr2.length; i++) {
         if (arr1[i] == arr2[i]) {
           console.log(arr1[i] + " is equals to " + arr2[i]);
-          $('#num' + i).addClass('correct');
+          $('#num' + i).addClass('correct').removeClass('wrong');
           $('#num' + i).attr("readonly", true);
-        }else{
+        } else {
           console.log(arr1[i] + " is not equals to " + arr2[i]);
           $('#num' + i).addClass('wrong');
         }
       }
-    }else{
+    } else {
       console.log("Array not filled");
     }
   }
 }
+
+function checkWin() {
+  if (arr1.length !== arr2.length)
+    console.log("Array not completed");
+  for (var i = arr1.length; i--;)
+    if (arr1[i] !== arr2[i]){
+      console.log(arr2[i] + " is not the same as" + arr1[i])
+  }
+  console.log("Both arrays same, you win!");
+}
+
+//reset code here
