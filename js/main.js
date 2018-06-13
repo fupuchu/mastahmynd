2f3a22460a96ce5c5db5bf135c9ea36ae939439e
Leon Ho <leonhwh@icloud.com>
2018-06-13 11:41:54 +0800

    changelog:
    finish win checking function()
    add validator
    working on resetGame()

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var ansObj = {
   num2 : Math.floor(Math.random() * 10).toString(),
   num3 : Math.floor(Math.random() * 10).toString()
 }
+
 var userGuess = {
   num0 : null,
   num1 : null,
@@ -25,25 +26,28 @@ function pushToArr1(){
 
 pushToArr1();
 
-function checkAnswer(){
+var wrongInput = 0;
+function checkAnswer(event){
+  attempts++;
+  event.preventDefault();
   arr2 = [];
   Object.keys(mainGuess).forEach(function(num){
     var iterated = mainGuess[num];
     console.log(iterated.value);
-    arr2.push(iterated.value);
+    if (iterated.value < 10 && iterated.value >= 0) {
+      arr2.push(iterated.value);
+    }else {
+      arr2.push("X");
+      console.log("Wrong input for " + mainGuess[num].id);
+      wrongInput++;
+      console.log("Wrong Input Counter " + wrongInput);
+    }
+    compareArray();
   })
   function historicGuess(){
     var historyStr = arr2.join("");
     $('#history').appendTo
   }
-  compareArray();
-}
-
-var userGuess = {
-  num0 : null,
-  num1 : null,
-  num2 : null,
-  num3 : null
 }
 
 function attemptCheck(x){
@@ -55,4 +59,25 @@ function attemptCheck(x){
 }
 function changeAttr(y){
   $('#' + y).attr("readonly", true);
+}
+
+function resetGame(){
+  ansObj = {
+    num0 : Math.floor(Math.random() * 10).toString(),
+    num1 : Math.floor(Math.random() * 10).toString(),
+    num2 : Math.floor(Math.random() * 10).toString(),
+    num3 : Math.floor(Math.random() * 10).toString()
+  }
+  //foreach push new value
+  userGuess = {
+    num0 : null,
+    num1 : null,
+    num2 : null,
+    num3 : null
+  }
+  //reset all values to null
+  attempts = 0;
+  // reset attempts
+  //put the button back
+  $('.btn').hide();
 }
--- js/win.js
+++ js/win.js
@@ -1,17 +1,20 @@
 var arr1 = [];
 var arr2 = [];
 
-var attempts = 0;
-
+var attempts = 9;
+var winCounter = 0;
 function compareArray() {
-  attempts++;
   var x = arr2.join("")
-  $("#history").append("<span>" + x + "</span>")
   $("#attemptBox").text(attempts);
+  if (wrongInput > 0) {
+    console.log("Replace the wrong input");
+    attempts = attempts - 1;
+  }
   if (attempts === 10) {
     console.log("You lose...");
-    $('.btn').remove();
+    $('.btn').hide();
   } else {
+    wrongInput = 0;
     if (arr1.length == arr2.length) {
       for (var i = 0; i < arr2.length; i++) {
         if (arr1[i] == arr2[i]) {
@@ -23,20 +26,24 @@ function compareArray() {
           $('#num' + i).addClass('wrong');
         }
       }
-    } else {
-      console.log("Array not filled");
+      $("#history").append("<span>" + x + "</span>")
+      checkWin();
     }
   }
 }
-
 function checkWin() {
   if (arr1.length !== arr2.length)
-    console.log("Array not completed");
-  for (var i = arr1.length; i--;)
-    if (arr1[i] !== arr2[i]){
-      console.log(arr2[i] + " is not the same as" + arr1[i])
+    console.log("Don't check");
+  for (var i = arr1.length; i--;){
+    if (arr1[i] !== arr2[i]) {
+      console.log("arrays not same")
+      break;
+    }else if (arr1[i] === arr2[i]) {
+      console.log("You win!");
+      winCounter++
+    }
   }
-  console.log("Both arrays same, you win!");
+  if (winCounter === 4) {
+    console.log("BIG WINNER!!!");
+  }
 }
-
-//reset code here
