4a7095360821ab2fa2a4ae59f0f1a1a19e52f09b
Leon Ho <leonhwh@icloud.com>
2018-06-13 16:07:41 +0800

    v0.1 changelog
    basic functions done/checked
    added styling

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ var userGuess = {
 }
 
 var mainGuess = document.querySelectorAll('.numGuess');
-$("#attemptBox").text(attempts);
+$("#attempCounter").text(attempts);
 
 console.log(ansObj);
 
@@ -28,56 +28,34 @@ pushToArr1();
 
 var wrongInput = 0;
 function checkAnswer(event){
-  attempts++;
+  attempts+= 1;
   event.preventDefault();
   arr2 = [];
   Object.keys(mainGuess).forEach(function(num){
     var iterated = mainGuess[num];
     console.log(iterated.value);
-    if (iterated.value < 10 && iterated.value >= 0) {
+    if (iterated.value < 10 && iterated.value >= 0 && iterated.value.length !== 0) {
       arr2.push(iterated.value);
     }else {
-      arr2.push("X");
-      console.log("Wrong input for " + mainGuess[num].id);
-      wrongInput++;
-      console.log("Wrong Input Counter " + wrongInput);
+      arr2.push("n");
+      wrongInput += 1;
     }
     compareArray();
   })
-  function historicGuess(){
-    var historyStr = arr2.join("");
-    $('#history').appendTo
-  }
-}
-
-function attemptCheck(x){
-  if ( x === 'correct') {
-    console.log("Don't Change it");
-  }else {
-    console.log("Change this");
-  }
 }
 function changeAttr(y){
   $('#' + y).attr("readonly", true);
 }
 
 function resetGame(){
-  ansObj = {
-    num0 : Math.floor(Math.random() * 10).toString(),
-    num1 : Math.floor(Math.random() * 10).toString(),
-    num2 : Math.floor(Math.random() * 10).toString(),
-    num3 : Math.floor(Math.random() * 10).toString()
-  }
-  //foreach push new value
-  userGuess = {
-    num0 : null,
-    num1 : null,
-    num2 : null,
-    num3 : null
-  }
-  //reset all values to null
+  Object.keys(ansObj).forEach(function(num){
+    ansObj[num] = Math.floor(Math.random() * 10).toString()
+  });
+  // Object.keys(userGuess).forEach(function(num){
+  //   userGuess[num] = null,
+  // });
   attempts = 0;
   // reset attempts
   //put the button back
-  $('.btn').hide();
+  $('.btn').show();
 }
--- js/win.js
+++ js/win.js
@@ -1,14 +1,24 @@
 var arr1 = [];
 var arr2 = [];
 
-var attempts = 9;
+var attempts = 0;
 var winCounter = 0;
+
 function compareArray() {
   var x = arr2.join("")
-  $("#attemptBox").text(attempts);
+
+  if (arr2.length === 4) {
+    console.log('arr2 length is 4');
+    if (arr2.includes("n")) {
+      attempts -= 1;
+    }
+    $("#attemptCounter").text(attempts);
+  };
+
   if (wrongInput > 0) {
+    //put in html element to say there was a wrong input
     console.log("Replace the wrong input");
-    attempts = attempts - 1;
+    console.log("current attempts " + attempts);
   }
   if (attempts === 10) {
     console.log("You lose...");
@@ -18,32 +28,29 @@ function compareArray() {
     if (arr1.length == arr2.length) {
       for (var i = 0; i < arr2.length; i++) {
         if (arr1[i] == arr2[i]) {
-          console.log(arr1[i] + " is equals to " + arr2[i]);
           $('#num' + i).addClass('correct').removeClass('wrong');
           $('#num' + i).attr("readonly", true);
         } else {
-          console.log(arr1[i] + " is not equals to " + arr2[i]);
           $('#num' + i).addClass('wrong');
         }
       }
-      $("#history").append("<span>" + x + "</span>")
+      $("#history").text(x);
       checkWin();
     }
   }
 }
 function checkWin() {
-  if (arr1.length !== arr2.length)
-    console.log("Don't check");
   for (var i = arr1.length; i--;){
     if (arr1[i] !== arr2[i]) {
       console.log("arrays not same")
       break;
     }else if (arr1[i] === arr2[i]) {
       console.log("You win!");
-      winCounter++
+      winCounter += 1;
     }
   }
   if (winCounter === 4) {
     console.log("BIG WINNER!!!");
+    $('.btn').hide();
   }
 }
